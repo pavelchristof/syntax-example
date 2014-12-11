205dc993e941ad4cb2e720a908f458ec8b65a56d
Paweł Nowak <pawel834@gmail.com>
2014-12-11 01:48:56 +0100

    Update to syntax 1.

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -6,14 +8,14 @@ import           Control.Applicative
 import           Control.Lens.Cons
 import           Control.Lens.SemiIso
 import           Control.Lens.TH
+import           Control.SIArrow
 import qualified Data.Attoparsec.Text.Lazy as AP
 import           Data.Char
 import           Data.Scientific (Scientific)
-import           Data.SemiIsoFunctor
-import           Data.Syntax (Syntax)
+import           Data.Syntax (Syntax, Seq)
 import qualified Data.Syntax as S
 import qualified Data.Syntax.Attoparsec.Text.Lazy as S
-import           Data.Syntax.Char (SyntaxChar)
+import           Data.Syntax.Char (SyntaxChar, SyntaxText)
 import qualified Data.Syntax.Char as S
 import qualified Data.Syntax.Combinator as S
 import qualified Data.Syntax.Printer.Text as S
@@ -39,30 +41,30 @@ data AST = Var Text
 $(makePrisms ''AST)
 
 -- | A variable name.
-name :: Syntax syn Text => syn Text
+name :: SyntaxText syn => syn () Text
 name = _Cons /$/ S.satisfy isAlpha /*/ S.takeWhile isAlphaNum
 
 -- | A quoted string.
-quoted :: SyntaxChar syn seq => syn seq
+quoted :: SyntaxChar syn => syn () (Seq syn)
 quoted = S.char '"' */ S.takeTill (=='"') /* S.char '"'
 
 -- | Encloses a symbol in parentheses.
-parens :: SyntaxChar syn seq => syn a -> syn a
+parens :: SyntaxChar syn => syn () a -> syn () a
 parens m = S.char '(' */ S.spaces_ */ m /* S.spaces_ /* S.char ')'
 
 -- | A literal.
-literal :: SyntaxChar syn Text => syn Literal
+literal :: SyntaxText syn => syn () Literal
 literal =  _LitNum /$/ S.scientific
-       /|/ _LitStr /$/ quoted
+       /+/ _LitStr /$/ quoted
 
 -- | An atom is a variable, literal or an expression in parentheses.
-atom :: SyntaxChar syn Text => syn AST
+atom :: SyntaxText syn => syn () AST
 atom =  _Lit /$/ literal
-    /|/ _Var /$/ name
-    /|/ parens expr
+    /+/ _Var /$/ name
+    /+/ parens expr
 
 -- | Parses a list of atoms and folds them with the _App prism.
-apps :: SyntaxChar syn Text => syn AST
+apps :: SyntaxText syn => syn () AST
 apps = bifoldl1 _App /$/ S.sepBy1 atom S.spaces1
 
 -- | An expression of our lambda calculus.
@@ -70,20 +72,20 @@ apps = bifoldl1 _App /$/ S.sepBy1 atom S.spaces1
 -- Thanks to 'tuple-morph' we don't have to worry about /* and */ here.
 -- Tuples are reassociated and units are removed by the 'morphed'
 -- isomorphism (applied in /$~ operator).
-expr :: SyntaxChar syn Text => syn AST
+expr :: SyntaxText syn => syn () AST
 expr =  _Abs /$~ S.char '\\'    /*/ S.spaces_
              /*/ name           /*/ S.spaces
              /*/ S.string "->"  /*/ S.spaces
              /*/ expr
 
-    /|/ _Let /$~ S.string "let" /*/ S.spaces1
+    /+/ _Let /$~ S.string "let" /*/ S.spaces1
              /*/ name           /*/ S.spaces
              /*/ S.char '='     /*/ S.spaces
              /*/ expr           /*/ S.spaces1
              /*/ S.string "in"  /*/ S.spaces1
              /*/ expr
 
-    /|/ apps
+    /+/ apps
 
 main :: IO ()
 main = do
@@ -91,7 +93,7 @@ main = do
     t <- T.getContents
 
     -- Try to parse it.
-    case AP.parse (S.getParser expr <* AP.skipSpace <* AP.endOfInput) t of
+    case AP.parse (S.getParser_ expr <* AP.skipSpace <* AP.endOfInput) t of
       AP.Fail _ _ err  -> putStrLn err
       AP.Done _ ast -> do
           -- If parsing succeeded print the AST.
@@ -99,7 +101,7 @@ main = do
 
           -- Try to pretty print it.
           -- (Printing cannot really fail in this example)
-          case S.runPrinter expr ast of
+          case S.runPrinter_ expr ast of
             Left err  -> putStrLn err
             Right bld -> T.putStrLn (T.toLazyText bld)
 
